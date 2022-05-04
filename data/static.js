c0abac3d2eaf06ac29e8fa25153875f460f18e0c
nghia191 <mai.nam.nghia.bunbu@gmail.com>
2022-05-04 17:22:29 +0700

    first commit

---
+++ data/static.js
@@ -0,0 +1,38 @@
+const books = [
+  {
+    id: 1,
+    name: "Nhat ky trong tu",
+    genre: "Tho",
+    authorId: 1,
+  },
+  {
+    id: 2,
+    name: "Ngam trang",
+    genre: "Tho",
+    authorId: 1,
+  },
+  {
+    id: 3,
+    name: "De men phieu luu ky",
+    genre: "Phieu luu",
+    authorId: 2,
+  },
+];
+
+const authors = [
+  {
+    id: 1,
+    name: "Ho Chi Minh",
+    age: 69,
+  },
+  {
+    id: 2,
+    name: "Vu Trong Phung",
+    age: 61,
+  },
+];
+
+module.exports = {
+  books,
+  authors,
+};
